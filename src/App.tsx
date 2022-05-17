0529e344a499c5fd4d9b47d2b295c62a088b8441
A. Koepcke <akoepcke@users.noreply.github.com>
2022-05-17 14:53:30 +0200

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div className="bg-blue-600">Test</div>;
+  return (
+    <div className="relative h-full bg-red-500">
+      <div className="w-10 h-full bg-green-300"></div>
+    </div>
+  );
 }
 
 export default App;
