b43cd44d765fe442cbd5adae7c3ff4c0a819d2fa
A. Koepcke <akoepcke@users.noreply.github.com>
2022-05-18 11:18:25 +0200

    add toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import clsx from "clsx";
+import { useState } from "react";
+
 function App() {
+  const [enabled, setEnabled] = useState(false);
+
   return (
-    // add or remove overflow-scroll when div is/is not restricted to parentContainer
-    <div className="h-full bg-red-500">
-      {/* <div className="h-full overflow-scroll bg-red-500"> */}
+    <div className={clsx("h-full bg-blue-200", { "overflow-scroll": enabled })}>
+      <button
+        className="absolute w-40 p-4 bg-white rounded shadow top-2 right-2"
+        onClick={() => {
+          setEnabled(!enabled);
+        }}
+      >
+        {enabled ? "Disable scrolling" : "Enable scrolling"}
+      </button>
+
       <div className="w-10 bg-green-300 h-2/3"></div>
       <div className="w-10 bg-green-500 h-2/3"></div>
     </div>
